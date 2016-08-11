cbdb73508b5fd4b66acccb9450cfb0b810054ff5
Solomon Tompkns <solomon@solmontompkins.com>
2016-08-10 21:09:34 -0500

    Generated Profile Model

--- todolists/db/schema.rb
+++ todolists/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811014319) do
+ActiveRecord::Schema.define(version: 20160811014740) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "gender"
+    t.integer  "birth_year"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
